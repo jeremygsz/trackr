f8130688a6fc5ed0ee11c91879898a6014d66d9c
jeremygsz <jeremygrosz@gmail.com>
2026-04-18 18:31:50 +0200

    vercel compatibility route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
-import { handlers } from "@/lib/auth"
-
-export const { GET, POST } = handlers
+// app/api/auth/[...nextauth]/route.ts
 export const runtime = 'nodejs'
 export const dynamic = 'force-dynamic'
+
+import { handlers } from "@/lib/auth"
+export const { GET, POST } = handlers
